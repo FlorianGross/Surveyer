8088f65cd9852ba7100dc593a88ccd17612d72e6
FlorianGross <Magic_LP@web.de>
2022-03-22 15:35:11 +0100

    Refactoring from the main part

--- index.js
+++ index.js
@@ -6,83 +6,81 @@ app.use(cors);
 
 const mongoose = require("mongoose");
 const internal = require('stream');
-const  { Schema } = mongoose;
+const { Schema } = mongoose;
 
 const sessionSchema = new Schema({
     owner: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "User"
     },
-    participants: Array,
+    participants: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    }],
+    isActive: Boolean,
 });
 const userSchema = new Schema({
     userName: String,
+    email: String,
+    password: String,
 })
 const surveySchema = new Schema({
     surveySession: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "Sessions"
     },
+    creator: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
     surveyOpened: Boolean,
     surveyName: String,
     surveyApprove: Number,
     surveyDeny: Number,
     surveyNotParicipate: Number,
-    surveyOptions: Array,
-    surveyOptionResults: Array,
+    surveyOptions: [String],
+    surveyOptionResults: [{ Number, Number, Number }],
+    participants: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }]
 });
 
 
-mongoose.connect('mongodb://root:example@localhost:27018', {useNewUrlParser: true, useUnifiedTopology: true, dbName: "android"}).then(()=>{
-    console.log("Successfully connected to MongoDB")}).catch(err=>console.log("Error connecting to DB: " + err));
+mongoose.connect('mongodb://root:example@localhost:27018', { useNewUrlParser: true, useUnifiedTopology: true, dbName: "android" }).then(() => {
+    console.log("Successfully connected to MongoDB")
+}).catch(err => console.log("Error connecting to DB: " + err));
 var Session = mongoose.model('Sessions', sessionSchema);
 var Surveys = mongoose.model('Surveys', surveySchema);
 var User = mongoose.model('User', userSchema);
 
 const server = require('http').createServer(app);
 
-const wss = new WebSocket.Server({server:server});
+const wss = new WebSocket.Server({ server: server });
 wss.on('connection', async (ws) => {
     console.log("New Client Connected");
-    var user = await User.create({
-        userName: Math.random.toString
-    });
-    console.log("User registered: " + user._id);
-    ws.send(JSON.stringify({ "Type": "InitAnswer", "uid": user._id}));
-    
     ws.on('message', async (message) => {
         console.log('received: %s', message);
         var obj = JSON.parse(message);
-        switch(obj.Type){
-            case "startSession":{
-                try{
-                var usertmp = await User.findById(obj.uid)
-                var sessionTemp = await Session.create({
-                    owner: usertmp,
-                    participants: [usertmp],
-                });
-                var answer = {
-                    "Type": "Answer",
-                    "sessionId": sessionTemp._id
-                }
-                ws.send(JSON.stringify(answer));
-            }catch(e){
-                ws.send(JSON.stringify({"Type":"Error", "Message": e}));
-                console.log("Error in startSession" + e);
+        switch (obj.Type) {
+            case "registerClient": {
+                registerClient(obj, ws);
+                break;
             }
+            case "startSession": {
+                startSession(obj, ws);
                 break;
             }
             case "stopSession": {
-                var answer = {
-                    "Type": "Answer",
-                    "Result": "Successful"
-                }
+                stopSession(obj, ws);
+                break;
+            }
+            case "startSurvey": {
+                startSurvey(obj, ws);
+                break;
+            }
+            case "stopSurvey": {
+                stopSurvey(obj, ws);
                 break;
             }
             default: break;
         }
-    }); 
-    ws.on('close', ()=>{
+    });
+    ws.on('close', () => {
         console.log("Client disconnected")
     })
 });
@@ -90,3 +88,65 @@ wss.on('connection', async (ws) => {
 server.listen(process.env.PORT || 3000, () => {
     console.log(`Server started on port ${server.address().port}`);
 });
+
+async function startSession(obj, ws) {
+    try {
+        var usertmp = await User.findById(obj.uid)
+        var sessionTemp = await Session.create({
+            owner: usertmp,
+            participants: [usertmp],
+            isActive: true
+        });
+        var answer = {
+            "Type": "Answer",
+            "sessionId": sessionTemp._id
+        }
+        ws.send(JSON.stringify(answer));
+    } catch (e) {
+        ws.send(JSON.stringify({ "Type": "Error", "Message": e }));
+        console.log("Error in startSession" + e);
+    }
+}
+
+async function stopSession(obj, ws) {
+    try {
+        currentSession = await Session.findById(obj.sessionId);
+        currentSession.isActive = false;
+        await currentSession.save();
+        var answer = {
+            "Type": "Answer",
+            "Result": "Session stopped Successful"
+        }
+    } catch (e) {
+        console.log(e)
+        var answer = {
+            "Type": "Error",
+            "Result": e
+        }
+    }
+    ws.send(JSON.stringify(answer));
+}
+
+async function startSurvey(obj, ws) {
+
+}
+async function stopSurvey(obj) {
+
+}
+async function registerClient(obj, ws) {
+    try {
+        var user = await User.create({});
+        var answer = {
+            "Type": "Answer",
+            "Result": "Client registered Successful",
+            "uid": user._id
+        };
+    } catch (e) {
+        console.log(e)
+        var answer = {
+            "Type": "Error",
+            "Result": e
+        }
+    }
+    ws.send(JSON.stringify(answer));
+}
